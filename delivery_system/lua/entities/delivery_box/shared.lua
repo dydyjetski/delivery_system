b7b5f3b5ea22e55757cb5762f3a28842f716a006
dydy <131807342+dydyjetski@users.noreply.github.com>
2025-07-13 17:32:10 +0200

    Fix erreur lua
    Fix erreur lua :
    
    
    [delivery_system] addons/delivery_system/lua/entities/npc_delivery/cl_init.lua:16: attempt to index global 'NPC_ARMORY' (a nil value)
    
     1. unknown - addons/delivery_system/lua/entities/npc_delivery/cl_init.lua:16

---
+++ delivery_system/lua/entities/delivery_box/shared.lua
@@ -0,0 +1,8 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+
+ENT.PrintName = "Delivery Box"
+ENT.Author = "Didi.IN7"
+ENT.Category = "Delivery System"
+ENT.Spawnable = false
+ENT.AdminSpawnable = false 
---
+++ delivery_system/lua/entities/npc_delivery/shared.lua
@@ -0,0 +1,8 @@
+ENT.Type = "ai"
+ENT.Base = "base_ai"
+
+ENT.PrintName = "NPC Delivery System"
+
+ENT.Spawnable = true
+ENT.Category = "Delivery System"
+
