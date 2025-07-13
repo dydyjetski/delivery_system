df33f113dcd82ef06f3350512ae22672e21f5ea7
dydy <131807342+dydyjetski@users.noreply.github.com>
2025-07-13 17:31:22 +0200

    Delete lua directory

--- lua/delivery_system/server/sv_network.lua
+++
@@ -1,4 +0,0 @@
-util.AddNetworkString("DELIVERY:UpdateCache")
-util.AddNetworkString("DIDI:OPEN_DELIVERY_SYSTEM_NPC")
-util.AddNetworkString("DIDI:START_DELIVERY")
-util.AddNetworkString("DIDI:SELL_PACKAGES")
--- lua/entities/npc_delivery/shared.lua
+++
@@ -1,8 +0,0 @@
-ENT.Type = "ai"
-ENT.Base = "base_ai"
-
-ENT.PrintName = "NPC Delivery System"
-
-ENT.Spawnable = true
-ENT.Category = "Delivery System"
-
